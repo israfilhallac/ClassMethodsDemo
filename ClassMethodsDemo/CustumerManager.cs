f19408d86a5c5f6f9bc41ceec9ee917f0a17025c
israfilhallac <i.hallac1408@gmail.com>
2021-02-21 16:34:09 +0200

    Add project files.

---
+++ ClassMethodsDemo/CustumerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodsDemo
+{
+    class CustumerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi :" +customer.Name);
+        }
+        public void Listing(Customer customer)
+        {
+            Console.WriteLine("Müşteri Listelendi :" +customer.SurName);
+        }
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi :" +customer.Id);
+        }
+    }
+}
---
+++ ClassMethodsDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMethodsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "İsrafil";
+            customer1.SurName = "Hallaç";
+            
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Sezgin";
+            customer2.SurName = "Hallaç";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müşterinin Adı: "+ customer.Name +"---------" + "Müşterinin SoyAdı: " + customer.SurName);
+                
+            }
+            Console.WriteLine("-------------------------------------");
+            CustumerManager custumerManager = new CustumerManager();
+            custumerManager.Add(customer1);
+            custumerManager.Remove(customer2);
+            custumerManager.Listing(customer1);
+
+
+        }
+    }
+}
